427b31ad3210ab7ec09cc9d3288195cf4b9b1721
OmarDIOP0 <odiop6170@gmail.com>
2025-01-09 16:56:01 +0000

    getAll customer using repository

--- src/main/java/com/omardiop/Main.java
+++ src/main/java/com/omardiop/Main.java
@@ -1,19 +1,29 @@
 package com.omardiop;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/v1/customers")
 public class Main {
+    
+    private final CustomerRepository customerRepository;
+
+    Main(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hello World!,its Omar DIOP";
+    
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 }
