6abf1dcb6bec8864ac8da0a196e6f339c83bb857
OmarDIOP0 <odiop6170@gmail.com>
2025-01-09 22:36:44 +0000

    update customer

--- src/main/java/com/omardiop/Main.java
+++ src/main/java/com/omardiop/Main.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,14 @@ public class Main {
     @DeleteMapping("{customerId}")
     public void deleteCustomer(@PathVariable("customerId") Integer customerId){
         customerRepository.deleteById(customerId);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer customerId,@RequestBody NewCustomerRequest request){
+        Customer customer = customerRepository.findById(customerId).orElseThrow();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
     }
 }
